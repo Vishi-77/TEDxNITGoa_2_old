dbda347d4790d34ebc56ea08e7db58cac8385f8a
Vishi-77 <vishalpratapofc@gmail.com>
2024-10-17 12:22:17 +0530

    a

--- src/Home.js
+++ src/Home.js
@@ -1,63 +1,94 @@
 const Home = () => {
-    return (
+  return (
+    <>
       <p>
-      This paragraph
-      contains a lot of lines
-      in the source code,
-      but the browser 
-      ignores it.
-      This paragraph
-      contains      a lot of spaces
-      in the source     code,
-      but the    browser 
-      ignores it
-      The number of lines in a paragraph depends on the size of the browser window. If you resize the browser window, the number of lines in this paragraph will change.
-      This paragraph
-      contains      a lot of spaces
-      in the source     code,
-      but the    browser 
-      ignores it.This paragraph
-      contains      a lot of spaces
-      in the source     code,
-      but the    browser 
-      ignores it.
-      This paragraph
-      contains      a lot of spaces
-      in the source     code,
-      but the    browser 
-      ignores it.This paragraph
-      contains      a lot of spaces
-      in the source     code,
-      but the    browser 
-      ignores it.This paragraph
-      contains      a lot of spaces
-      in the source     code,
-      but the    browser 
-      ignores it.This paragraph
-      contains      a lot of spaces
-      in the source     code,
-      but the    browser 
-      ignores it.This paragraph
-      contains      a lot of spaces
-      in the source     code,
-      but the    browser 
-      ignores it.This paragraph
-      contains      a lot of spaces
-      in the source     code,
-      but the    browser 
-      ignores it.This paragraph
-      contains      a lot of spaces
-      in the source     code,
-      but the    browser 
-      ignores it.This paragraph
-      contains      a lot of spaces
-      in the source     code,
-      but the    browser 
-      ignores it.
+        This paragraph
+        contains a lot of lines
+        in the source code,
+        but the browser 
+        ignores it.
+        This paragraph
+        contains      a lot of spaces
+        in the source     code,
+        but the    browser 
+        ignores it
+        The number of lines in a paragraph depends on the size of the browser window. If you resize the browser window, the number of lines in this paragraph will change.
+        This paragraph
+        contains      a lot of spaces
+        in the source     code,
+        but the    browser 
+        ignores it.This paragraph
+        contains      a lot of spaces
+        in the source     code,
+        but the    browser 
+        ignores it.
+        This paragraph
+        contains      a lot of spaces
+        in the source     code,
+        but the    browser 
+        ignores it.This paragraph
+        contains      a lot of spaces
+        in the source     code,
+        but the    browser 
+        ignores it.This paragraph
+        contains      a lot of spaces
+        in the source     code,
+        but the    browser 
+        ignores it.This paragraph
+        contains      a lot of spaces
+        in the source     code,
+        but the    browser 
+        ignores it.This paragraph
+        contains      a lot of spaces
+        in the source     code,
+        but the    browser 
+        ignores it.This paragraph
+        contains      a lot of spaces
+        in the source     code,
+        but the    browser 
+        ignores it.This paragraph
+        contains      a lot of spaces
+        in the source     code,
+        but the    browser 
+        ignores it.
       </p>
-      
 
-      );
+      <div className="container">
+        <div className="card">
+          <div className="box">
+            <div className="content">
+              <h2>01</h2>
+              <h3>Card One</h3>
+              <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Labore, totam velit? Iure nemo labore inventore?</p>
+              <a href="#">Read More</a>
+            </div>
+          </div>
+        </div>
+
+        <div className="card">
+          <div className="box">
+            <div className="content">
+              <h2>02</h2>
+              <h3>Card Two</h3>
+              <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Labore, totam velit? Iure nemo labore inventore?</p>
+              <a href="#">Read More</a>
+            </div>
+          </div>
+        </div>
+
+        <div className="card">
+          <div className="box">
+            <div className="content">
+              <h2>03</h2>
+              <h3>Card Three</h3>
+              <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Labore, totam velit? Iure nemo labore inventore?</p>
+              <a href="#">Read More</a>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
 }
- 
+
 export default Home;
